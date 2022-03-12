1245966560c9855fcc75cf25cfd919d67e0b20c8
hugosenari <hugosenari@gmail.com>
2022-03-12 02:55:39 -0300

    chore: re index georefar

---
+++ 0_1_5/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''GeoRef Argentina Government MultiSync API Client for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-georefar-0_1_5.flake = false;
+  inputs.src-georefar-0_1_5.owner = "juancarlospaco";
+  inputs.src-georefar-0_1_5.ref   = "refs/tags/0.1.5";
+  inputs.src-georefar-0_1_5.repo  = "nim-georefar";
+  inputs.src-georefar-0_1_5.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-georefar-0_1_5"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-georefar-0_1_5";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
