1711f6d8b948949eec4082fe2dbef39eb664281d
hugosenari <hugosenari@gmail.com>
2022-03-27 21:49:54 -0300

    chore: re index georefar

--- 0_1_5/flake.nix
+++ 0_1_5/flake.nix
@@ -11,7 +11,6 @@
   inputs.src-georefar-0_1_5.ref   = "refs/tags/0.1.5";
   inputs.src-georefar-0_1_5.owner = "juancarlospaco";
   inputs.src-georefar-0_1_5.repo  = "nim-georefar";
-  inputs.src-georefar-0_1_5.dir   = "";
   inputs.src-georefar-0_1_5.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
